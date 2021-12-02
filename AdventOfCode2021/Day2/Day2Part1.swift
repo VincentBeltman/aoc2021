69707ff9761880d93e3e716855a2b38ddbf78fba
Vincent Beltman <vincent.beltman@inspiro.nl>
2021-12-02 12:57:30 +0100

    Day2

---
+++ AdventOfCode2021/Day2/Day2Part1.swift
@@ -0,0 +1,43 @@
+//
+//  Day2Part1.swift
+//  AdventOfCode2021
+//
+//  Created by Inspiro developer on 02/12/2021.
+//
+
+import Foundation
+
+class Day2Part1: PartRunner
+{
+  override func doStuff(with input: String) -> String
+  {
+    let lines: [String.SubSequence] = input.split(separator: "\n")
+
+    log("Running part 1 of day 2")
+
+    var x: Int = 0
+    var y: Int = 0
+
+    for line in lines
+    {
+      let lineParts: [String.SubSequence] = line.split(separator: " ")
+      switch lineParts[0]
+      {
+      case "forward":
+        x += Int(lineParts[1])!
+      case "down":
+        y += Int(lineParts[1])!
+      case "up":
+        y -= Int(lineParts[1])!
+      default:
+        assert(false, "Should never happen, pancake")
+      }
+    }
+
+    return "X: \(x) Y: \(y) Result \(x*y)"
+  }
+
+  override func cleanUp()
+  {
+  }
+}
--- AdventOfCode2021/main.swift
+++ AdventOfCode2021/main.swift
@@ -7,10 +7,20 @@
 
 import Foundation
 
-let dayPart1Runner: Day1Part1 = Day1Part1(verbose: false)
-dayPart1Runner.run(input: Day1Input.EXAMPLE)
-dayPart1Runner.run(input: Day1Input.PUZZLE_INPUT)
+// MARK: Day1
+//let dayPart1Runner: Day1Part1 = Day1Part1(verbose: false)
+//dayPart1Runner.run(input: Day1Input.EXAMPLE)
+//dayPart1Runner.run(input: Day1Input.PUZZLE_INPUT)
+//
+//let dayPart2Runner: Day1Part2 = Day1Part2(verbose: false)
+//dayPart2Runner.run(input: Day1Input.EXAMPLE)
+//dayPart2Runner.run(input: Day1Input.PUZZLE_INPUT)
 
-let dayPart2Runner: Day1Part2 = Day1Part2(verbose: false)
-dayPart2Runner.run(input: Day1Input.EXAMPLE)
-dayPart2Runner.run(input: Day1Input.PUZZLE_INPUT)
+// MARK: Day2
+let part1Runner: Day2Part1 = Day2Part1(verbose: true)
+part1Runner.run(input: Day2Input.EXAMPLE)
+part1Runner.run(input: Day2Input.PUZZLE_INPUT)
+
+let part2Runner: Day2Part2 = Day2Part2(verbose: true)
+part2Runner.run(input: Day2Input.EXAMPLE)
+part2Runner.run(input: Day2Input.PUZZLE_INPUT)
